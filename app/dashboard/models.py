da88db04ccd9845749d45bf3a764f55aa185fced
Qingyun Yang <qyang063@uottawa.ca>
2024-03-02 03:26:53 -0500

    friend list

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class EasyUser(models.Model):
+    name=models.CharField(max_length=255)
+    friends=models.CharField(max_length=2550)
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 import json
+from dashboard.models import EasyUser
 
 # Create your views here.
 def get_user(request):
     token=request.session.get("user")
 
-    return render(request,"dashboard.html",{"sub":token["userinfo"]["sub"]})
+    user_exists = EasyUser.objects.filter(name=token["userinfo"]["sub"]).exists()
+    if(user_exists):
+        user = EasyUser.objects.get(name=token["userinfo"]["sub"])
+    else:
+        user = EasyUser(name=token["userinfo"]["sub"], friends='Jane Doe, Mark Smith')
+        user.save()
+    
+    return render(request,"dashboard.html",{"sub":token["userinfo"]["sub"],"friends":user.friends})
