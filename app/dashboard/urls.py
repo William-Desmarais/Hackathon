a93f7a5d49be18d05160284482e9d98375c5650d
Qingyun Yang <qyang063@uottawa.ca>
2024-03-02 08:50:30 -0500

    nav dashboard

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('user/',views.get_user),
+    path('user/',views.get_user,name='get_user'),
     path('add_friend/', views.add_friend, name='add_friend'),
 ]
